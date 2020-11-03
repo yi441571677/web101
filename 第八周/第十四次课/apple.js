5a7cf9ba8953563a270ce8956fc13c3dfb994def
yi441571677 <441571677@qq.com>
2020-11-03 11:31:19 +0800

    第十四次课

---
+++ 第八周/第十四次课/apple.js
@@ -0,0 +1,8 @@
+function apple(){
+    var array = [12,67,89,100,34,56,78,99];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    }
+}
+apple();
